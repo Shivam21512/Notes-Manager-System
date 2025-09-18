21656d9bbe297326577fedf96c042fcf183e3262
Shivam21512 <ugaleshivam73@gmail.com>
2025-09-19 00:31:21 +0530

    final

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,13 +1,37 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+import mongoose from 'mongoose';
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB(uri) {
+  if (cached.conn) {
+    return cached.conn;
   }
-};
-
-module.exports = connectDB;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(uri, { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true 
+    }).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
